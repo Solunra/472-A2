0ad1ada0aa638a140e67d0a2675ed48609f0f990
WilliamM-L <w.morin.laberge@gmail.com>
2021-11-15 22:17:36 -0500

    Added missing run

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import skeleton_tictactoe as ttt
 
 
 def main():
-    ttt.main(game_size=4, blocks=4, block_positions=[(0, 0), (0, 3), (3, 0), (3, 3)], win_length=3, max_execution_time=5, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.MINIMAX)
-    ttt.main(game_size=4, blocks=4, block_positions=[(0, 0), (0, 3), (3, 0), (3, 3)], win_length=3, max_execution_time=5, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
-    ttt.main(game_size=5, blocks=4, win_length=4, max_execution_time=1, player_x_max_depth=2, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
-    ttt.main(game_size=5, blocks=4, win_length=4, max_execution_time=5, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
-    ttt.main(game_size=8, blocks=5, win_length=5, max_execution_time=1, player_x_max_depth=2, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
-    ttt.main(game_size=8, blocks=5, win_length=5, max_execution_time=5, player_x_max_depth=2, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
-    ttt.main(game_size=8, blocks=6, win_length=5, max_execution_time=1, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
-    ttt.main(game_size=8, blocks=6, win_length=5, max_execution_time=5, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
+    # ttt.main(game_size=4, blocks=4, block_positions=[(0, 0), (0, 3), (3, 0), (3, 3)], win_length=3, max_execution_time=5, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.MINIMAX)
+    ttt.main(game_size=4, blocks=4, block_positions=[(0, 0), (0, 3), (3, 0), (3, 3)], win_length=3, max_execution_time=1, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
+    # ttt.main(game_size=5, blocks=4, win_length=4, max_execution_time=1, player_x_max_depth=2, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
+    # ttt.main(game_size=5, blocks=4, win_length=4, max_execution_time=5, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
+    # ttt.main(game_size=8, blocks=5, win_length=5, max_execution_time=1, player_x_max_depth=2, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
+    # ttt.main(game_size=8, blocks=5, win_length=5, max_execution_time=5, player_x_max_depth=2, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
+    # ttt.main(game_size=8, blocks=6, win_length=5, max_execution_time=1, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
+    # ttt.main(game_size=8, blocks=6, win_length=5, max_execution_time=5, player_x_max_depth=6, player_o_max_depth=6, algo=ttt.Game.ALPHABETA)
 
 
 if __name__ == '__main__':
